07c443d620792eda68832b572ede1ac676f24eb3
Steven Portillo <stportillo83@gmail.com>
2018-04-12 14:10:00 -0400

    Made Html,  CSS and JS files and started the trivia game

---
+++ logic.js
@@ -0,0 +1,20 @@
+var panel = $("");
+var countStartNumber = 30;
+
+// Question set
+var questions = [
+{
+    question: "What happened first in Vault 111?",
+    answers: ["You were cryo-frozen", "You are killed", "You go to your room and talk to the family", "You died outside and never went inside the vault in the first place."],
+    correctAnswer: "You were cryo-frozen",
+    image: "assets/Cryo pod .jpg"
+},
+
+{
+    question: "What is the best weapon in Fallout 4?",
+    answers: ["Laser Rifle", "Aeternus (Unique Gatling Laser)", ".44 Magnum", "Combat Shotgun"],
+    correctAnswer: "Aeternus (Unique Gatling Laser)",
+    image: "assets/Aeternus.jpg"
+},
+
+]
